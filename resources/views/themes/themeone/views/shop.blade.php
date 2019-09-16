c0ecdd25057c62b20cf1284a6c9e0e9b0084267d
TemkaHD <TemkaHD@gmail.com>
2019-09-16 16:18:48 +0300

    fix

--- resources/views/themes/themeone/views/shop.blade.php
+++ resources/views/themes/themeone/views/shop.blade.php
@@ -130,9 +130,9 @@
 
                                                         @if(!empty($products->discount_price))
                                                             {{$web_setting[19]->value}}{{$products->discount_price+0}}
-                                                            <span> {{$web_setting[19]->value}}{{$products->products_price+0}}, {{$products->products_price_byn+0}} BYN</span>
+                                                           <small style="color: gray">{{$web_setting[19]->value}}{{$products->products_price+0}},</small> {{$products->products_price_byn+0}} BYN
                                                         @else
-                                                            {{$web_setting[19]->value}}{{$products->products_price+0}}, {{$products->products_price_byn+0}} BYN
+                                                           <small style="color: gray;">{{$web_setting[19]->value}}{{$products->products_price+0}},</small>  {{$products->products_price_byn+0}} BYN
                                                         @endif
                                                     </div>
 
